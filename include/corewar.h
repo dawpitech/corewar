842e69a5db7d0f657a43609f679c63faf6109ec6
Antoine <me@antoinebellanger.fr>
2024-04-26 10:25:11 +0200

    [FEATURE] Add name, and carry bit to each program

--- include/corewar.h
+++ include/corewar.h
@@ -15,10 +15,12 @@
     #define EXIT_SUCCESS_TECH 0
     #define EXIT_FAILURE_TECH 84
 typedef struct {
+    char *name;
     uint32_t program_counter;
     int32_t registers[REG_NUMBER];
     uint8_t cycles_before_next_instruction;
     uint8_t cycles_before_die;
+    uint8_t carry_bit;
     bool is_dead;
 } program_t;
 typedef struct {
