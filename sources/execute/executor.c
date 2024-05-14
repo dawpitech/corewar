6260fb5d1629b52876da9485ebb5019703de0144
Antoine <me@antoinebellanger.fr>
2024-05-14 13:58:52 +0200

    add: and instruction

--- sources/execute/executor.c
+++ sources/execute/executor.c
@@ -13,7 +13,6 @@
 #include "corewar.h"
 #include "my.h"
 #include "my_printf.h"
-#include "op.h"
 
 static
 int execute_live(arena_t *arena, program_t *program)
@@ -106,6 +105,29 @@ int execute_aff(arena_t *arena, program_t *program)
         return 1;
     chr = (char) (program->registers[infos->params[0].value - 1] % 256);
     write(STDOUT_FILENO, &chr, 1);
+    free(infos);
+    return 0;
+}
+
+static
+int execute_and(arena_t *arena, program_t *program)
+{
+    instruct_infos_t *infos = decode_instruction(arena, &program->program_counter);
+    int32_t a;
+    int32_t b;
+    int32_t and_result;
+
+    if (infos == NULL || infos->params[2].size != T_REG || infos->params[2].value > REG_NUMBER)
+        return 1;
+    a = infos->params[0].value;
+    b = infos->params[1].value;
+    if (infos->params[0].size == T_REG && infos->params[0].value <= REG_NUMBER)
+        a = program->registers[infos->params[0].value - 1];
+    if (infos->params[1].size == T_REG && infos->params[1].value <= REG_NUMBER)
+        b = program->registers[infos->params[1].value - 1];
+    and_result = a & b;
+    program->registers[infos->params[2].value - 1] = and_result;
+    free(infos);
     return 0;
 }
 
@@ -148,6 +170,8 @@ int execute_inst(int i, arena_t *arena, program_t *program)
         return execute_sti(arena, program);
     if (my_strcmp(op_tab[i].mnemonique, "aff") == 0)
         return execute_aff(arena, program);
+    if (my_strcmp(op_tab[i].mnemonique, "and") == 0)
+        return execute_and(arena, program);
     return EXIT_FAILURE_TECH;
 }
 
