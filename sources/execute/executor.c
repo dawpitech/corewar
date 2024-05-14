fe2f69051d5f87d75699b1fea22bcc709b25910e
ALittlePatate <maxime@patate.dev>
2024-05-14 13:18:43 +0200

    add: st instruction

--- sources/execute/executor.c
+++ sources/execute/executor.c
@@ -52,6 +52,30 @@ int execute_ld(arena_t *arena, program_t *program)
 }
 
 static
+int execute_st(arena_t *arena, program_t *program)
+{
+    uint32_t tmp = program->program_counter;
+    uint32_t val = 0;
+    instruct_infos_t *params = decode_instruction(arena,
+        &program->program_counter);
+
+    if (params == NULL)
+        return 1;
+    val = tmp + params->params[1].value % IDX_MOD;
+    if (params->params[1].size == T_DIR)
+        val = params->params[1].value;
+    if (params->params[1].size == T_REG) {
+        program->registers[params->params[1].value - 1] =
+            program->registers[params->params[0].value - 1];
+        free(params);
+        return 0;
+    }
+    write_bytes(program->registers[params->params[0].value], 4, val, arena);
+    free(params);
+    return 0;
+}
+
+static
 int execute_sti(arena_t *arena, program_t *program)
 {
     uint32_t tmp = program->program_counter;
@@ -101,6 +125,8 @@ int execute_inst(int i, arena_t *arena, program_t *program)
         return execute_live(arena, program);
     if (my_strcmp(op_tab[i].mnemonique, "ld") == 0)
         return execute_ld(arena, program);
+    if (my_strcmp(op_tab[i].mnemonique, "st") == 0)
+        return execute_st(arena, program);
     if (my_strcmp(op_tab[i].mnemonique, "sti") == 0)
         return execute_sti(arena, program);
     if (my_strcmp(op_tab[i].mnemonique, "aff") == 0)
