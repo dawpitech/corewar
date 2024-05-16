385d85fa21e78f293c44ffc708e98847727ffe99
Antoine <me@antoinebellanger.fr>
2024-05-16 11:45:20 +0200

    fix: read value and incrementation of the program counter

--- sources/decoder/instruction_decoder.c
+++ sources/decoder/instruction_decoder.c
@@ -7,22 +7,31 @@
 
 #include <endian.h>
 #include <malloc.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "corewar.h"
 
 static
-int read_value(arena_t *arena, uint32_t address, size_t size)
+int read_value(arena_t *arena, uint32_t *address, size_t type)
 {
-    switch (size) {
+    int rt_value;
+
+    switch (type) {
         case 1:
-            return (int) read_uint8(arena, address);
+            rt_value = read_uint8(arena, *address);
+            *address += 1;
+            return rt_value;
         case 2:
-            return (int) htobe16(read_uint16(arena, address));
+            rt_value = htobe32(read_uint32(arena, *address));
+            *address += 4;
+            return rt_value;
         case 4:
-            return (int) htobe32(read_uint32(arena, address));
+            rt_value = htobe16(read_uint16(arena, *address));
+            *address += 2;
+            return rt_value;
         default:
-            return 0;
+            return -1;
     }
 }
 
@@ -31,16 +40,16 @@ void read_params(arena_t *arena, uint32_t *address,
     instruct_infos_t *params, bool is_index)
 {
     uint8_t *params_type = decode_cb(arena, *address);
-    size_t size_to_read;
+    size_t param_type;
 
     *address += 1;
     for (int i = 0; i < 4; i++) {
-        size_to_read = params_type[i];
-        if (!is_index && size_to_read == T_DIR)
-            size_to_read = T_IND;
-        params->params[i].value = read_value(arena, *address, size_to_read);
-        params->params[i].size = size_to_read;
-        *address += size_to_read;
+        param_type = params_type[i];
+        printf("[DEBUG] DECODER: TYPE IS %zu\n", param_type);
+        if (is_index && param_type == T_DIR)
+            param_type = T_IND;
+        params->params[i].value = read_value(arena, address, param_type);
+        params->params[i].size = param_type;
     }
     free(params_type);
 }
